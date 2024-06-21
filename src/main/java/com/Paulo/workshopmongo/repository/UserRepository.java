07317e2bba5b9af819a008c87d66b3fd5cbd3eee
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-20 22:41:55 -0300

    Connecting to mongoDB with repository and service

---
+++ src/main/java/com/Paulo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Paulo.workshopmongo.repository;
+
+import com.Paulo.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/Paulo/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.Paulo.workshopmongo.services;
+
+import com.Paulo.workshopmongo.domain.User;
+import com.Paulo.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+}
