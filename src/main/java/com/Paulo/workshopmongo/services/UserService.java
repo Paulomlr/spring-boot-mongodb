009a3784bd11f839ba77cad175988d0567736fee
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-21 11:59:31 -0300

    User insertion with post

--- src/main/java/com/Paulo/workshopmongo/services/UserService.java
+++ src/main/java/com/Paulo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.Paulo.workshopmongo.services;
 
 import com.Paulo.workshopmongo.domain.User;
+import com.Paulo.workshopmongo.dto.UserDTO;
 import com.Paulo.workshopmongo.repository.UserRepository;
 import com.Paulo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
-    } 
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 }
