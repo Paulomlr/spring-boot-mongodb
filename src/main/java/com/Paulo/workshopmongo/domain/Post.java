663983da033a835bdd613bd2d30cec6023623e91
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-21 23:02:39 -0300

    Adding comments to posts

--- src/main/java/com/Paulo/workshopmongo/domain/Post.java
+++ src/main/java/com/Paulo/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.Paulo.workshopmongo.domain;
 
 import com.Paulo.workshopmongo.dto.AuthorDTO;
+import com.Paulo.workshopmongo.dto.CommentDTO;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "tb_post")
@@ -20,6 +23,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){}
 
@@ -69,6 +74,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
